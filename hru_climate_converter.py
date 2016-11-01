627a166c41c0fa5b5328b3bd97e9e45011229f6f
Micah Johnson <micah.johnson150@gmail.com>
2016-11-01 10:21:30 -0600

    Working conversion file. Produces necessary output

--- hru_climate_converter.py
+++ hru_climate_converter.py
@@ -23,37 +23,55 @@ class OnlineClimateFile(object):
         else:
             ValueError("\nExpected filename to be type str not {0}".format(type(filename)))
         
-        df = pd.read_csv(self.filename,parse_dates=True)
+        self.df = pd.read_csv(self.filename,parse_dates=True)
         
         #Gather and edit the date to match the format needs for prms
-    	for index, row in df.iterrows():
-            orig_date = df.get_value(index,"date")
+    	for index, row in self.df.iterrows():
+            orig_date = self.df.get_value(index,"date")
             month, day, year = orig_date.split("/")
-            prms_date = " ".join([year,month,day,"0","0", "0"])
-            df.set_value(index,"date",prms_date)
+            prms_date = ' '.join([year,month,day,'0','0', '0'])
+            self.df.set_value(index,"date",prms_date)
             
-        self.output_data_file(df,"tmin","RCEW_Tmin.data")
             
-        def output_data_file(self,df,col_str,filename):
-            """
-            Searches dataframe work for columns having the col_str,
-            creates a new data frame work with the date append at the front
-            of each line.
-            Writes the file in space delimited format to filename
-            """
-            df2 = pd.DataFrame(df["date"])
-            frames = []
-            frames.append(df2)
-            for name in list(df.columns.values):
-                if col_str in name:
-                    frames.append(df[name])
-            result = pd.concat(frames)
-            
-            result.to_csv(filename," ")
-
+    def output_data_file(self,col_str,filename):
+        """
+        Searches dataframe work for columns having the col_str,
+        creates a new data frame work with the date append at the front
+        of each line.
+        Writes the file in space delimited format to filename
+        """
+        df2 = pd.DataFrame(self. df["date"])
+        frames = []
+        frames.append(df2)
+        for name in list(self.df.columns.values):
+            header_name = name.split("[")
+            if col_str in header_name[0]:
+                #If we have a name match then add the name plus the associated hru i.e. tmin[23]
+                df2[name] = self.df[name]
+                
+        #Provide feedback is usr provided string was not found.
+        if len(list(df2.columns.values)) <= 1 :
+            print "\nWarning: Column name {0} was not found in the file.".format(col_str)
+        
+        else:
+            print "\nWriting {0} columns that contained the string {1}". format(len(list(df2.columns.values))-1,col_str)
+                    
+        with open(filename,'w') as f:
+            #Append the PRMS expected Header
+            f.write("########################################\n")
+            df2.to_csv(f,"\t", header = False, index = False)
+            f.close()
+        print "\tData file outputted to {0}".format(filename)
 
 if __name__=='__main__':
     my_file = "/home/micahjohnson/Documents/data-hru.csv"
     
     ui_file = OnlineClimateFile(my_file)
+    
+    ui_file.output_data_file("tmin","/home/micahjohnson/Documents/RCEW_Tmin.data")
+    ui_file.output_data_file("tmax","/home/micahjohnson/Documents/RCEW_Tmax.data")
+    ui_file.output_data_file("precip","/home/micahjohnson/Documents/RCEW_precip.data")
+    ui_file.output_data_file("swe","/home/micahjohnson/Documents/RCEW_swe.data")
+    
+
    
